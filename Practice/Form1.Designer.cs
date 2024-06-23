11eb056a26338418e34f5bd2d65854f4eff5d3fd
laragrace30 <laragrace.riconalla.20@usjr.edu.ph>
2024-06-23 20:47:21 +0800

    Added the MDAS

--- Practice/Form1.Designer.cs
+++ Practice/Form1.Designer.cs
@@ -46,6 +46,10 @@
             botSeven = new Button();
             botZero = new Button();
             button16 = new Button();
+            open = new Button();
+            close = new Button();
+            button1 = new Button();
+            decButton = new Button();
             SuspendLayout();
             // 
             // txtTest
@@ -54,61 +58,66 @@
             txtTest.Location = new Point(12, 12);
             txtTest.Name = "txtTest";
             txtTest.ReadOnly = true;
-            txtTest.Size = new Size(244, 43);
+            txtTest.Size = new Size(210, 43);
             txtTest.TabIndex = 2;
+            txtTest.TextAlign = HorizontalAlignment.Right;
             // 
             // add
             // 
             add.Font = new Font("Segoe UI", 15F);
-            add.ForeColor = Color.Red;
+            add.ForeColor = Color.Black;
             add.ImageAlign = ContentAlignment.TopRight;
-            add.Location = new Point(254, 100);
+            add.Location = new Point(198, 160);
             add.Name = "add";
             add.Size = new Size(52, 44);
             add.TabIndex = 3;
             add.Text = "+";
             add.UseVisualStyleBackColor = true;
+            add.MouseClick += add_MouseClick;
             // 
             // minus
             // 
             minus.Font = new Font("Segoe UI", 15F);
-            minus.ForeColor = Color.Red;
-            minus.Location = new Point(254, 150);
+            minus.ForeColor = Color.Black;
+            minus.Location = new Point(198, 210);
             minus.Name = "minus";
             minus.Size = new Size(52, 44);
             minus.TabIndex = 4;
             minus.Text = "-";
             minus.UseVisualStyleBackColor = true;
+            minus.MouseClick += minus_MouseClick;
             // 
             // multiply
             // 
             multiply.Font = new Font("Segoe UI", 15F);
-            multiply.ForeColor = Color.Red;
-            multiply.Location = new Point(254, 200);
+            multiply.ForeColor = Color.Black;
+            multiply.Location = new Point(254, 160);
             multiply.Name = "multiply";
             multiply.Size = new Size(52, 44);
             multiply.TabIndex = 5;
             multiply.Text = "*";
             multiply.UseVisualStyleBackColor = true;
+            multiply.MouseClick += multiply_MouseClick;
             // 
             // divide
             // 
             divide.Font = new Font("Segoe UI", 15F);
-            divide.ForeColor = Color.Red;
-            divide.Location = new Point(254, 250);
+            divide.ForeColor = Color.Black;
+            divide.Location = new Point(254, 210);
             divide.Name = "divide";
             divide.Size = new Size(52, 44);
             divide.TabIndex = 6;
             divide.Text = "/";
             divide.UseVisualStyleBackColor = true;
+            divide.MouseClick += divide_MouseClick;
             // 
             // button5
             // 
             button5.Font = new Font("Segoe UI", 15F);
-            button5.ForeColor = Color.Red;
-            button5.Location = new Point(192, 250);
+            button5.ForeColor = Color.Black;
+            button5.Location = new Point(254, 260);
             button5.Name = "button5";
-            button5.Size = new Size(56, 44);
+            button5.Size = new Size(52, 44);
             button5.TabIndex = 7;
             button5.Text = "=";
             button5.UseVisualStyleBackColor = true;
@@ -116,10 +125,10 @@
             // 
             // lblTest
             // 
-            lblTest.BorderStyle = BorderStyle.Fixed3D;
-            lblTest.Location = new Point(254, 12);
+            lblTest.BorderStyle = BorderStyle.FixedSingle;
+            lblTest.Location = new Point(222, 12);
             lblTest.Name = "lblTest";
-            lblTest.Size = new Size(117, 43);
+            lblTest.Size = new Size(84, 43);
             lblTest.TabIndex = 0;
             lblTest.Text = "..........";
             lblTest.TextAlign = ContentAlignment.MiddleCenter;
@@ -128,105 +137,114 @@
             // 
             botThree.Font = new Font("Segoe UI", 15F);
             botThree.ForeColor = Color.Black;
-            botThree.Location = new Point(192, 200);
+            botThree.Location = new Point(136, 210);
             botThree.Name = "botThree";
             botThree.Size = new Size(56, 44);
             botThree.TabIndex = 8;
             botThree.Text = "3";
             botThree.UseVisualStyleBackColor = true;
+            botThree.MouseClick += botThree_MouseClick;
             // 
             // botTwo
             // 
             botTwo.Font = new Font("Segoe UI", 15F);
             botTwo.ForeColor = Color.Black;
-            botTwo.Location = new Point(130, 200);
+            botTwo.Location = new Point(74, 210);
             botTwo.Name = "botTwo";
             botTwo.Size = new Size(56, 44);
             botTwo.TabIndex = 9;
             botTwo.Text = "2";
             botTwo.UseVisualStyleBackColor = true;
+            botTwo.MouseClick += botTwo_MouseClick;
             // 
             // botOne
             // 
             botOne.Font = new Font("Segoe UI", 15F);
             botOne.ForeColor = Color.Black;
-            botOne.Location = new Point(68, 200);
+            botOne.Location = new Point(12, 210);
             botOne.Name = "botOne";
             botOne.Size = new Size(56, 44);
             botOne.TabIndex = 10;
             botOne.Text = "1";
             botOne.UseVisualStyleBackColor = true;
+            botOne.MouseClick += botOne_MouseClick;
             // 
             // botSix
             // 
             botSix.Font = new Font("Segoe UI", 15F);
             botSix.ForeColor = Color.Black;
-            botSix.Location = new Point(192, 150);
+            botSix.Location = new Point(136, 160);
             botSix.Name = "botSix";
             botSix.Size = new Size(56, 44);
             botSix.TabIndex = 11;
             botSix.Text = "6";
             botSix.UseVisualStyleBackColor = true;
+            botSix.MouseClick += botSix_MouseClick;
             // 
             // botFive
             // 
             botFive.Font = new Font("Segoe UI", 15F);
             botFive.ForeColor = Color.Black;
-            botFive.Location = new Point(130, 150);
+            botFive.Location = new Point(74, 160);
             botFive.Name = "botFive";
             botFive.Size = new Size(56, 44);
             botFive.TabIndex = 12;
             botFive.Text = "5";
             botFive.UseVisualStyleBackColor = true;
+            botFive.MouseClick += botFive_MouseClick;
             // 
             // botFour
             // 
             botFour.Font = new Font("Segoe UI", 15F);
-            botFour.Location = new Point(68, 150);
+            botFour.Location = new Point(12, 160);
             botFour.Name = "botFour";
             botFour.Size = new Size(56, 44);
             botFour.TabIndex = 13;
             botFour.Text = "4";
             botFour.UseVisualStyleBackColor = true;
+            botFour.MouseClick += botFour_MouseClick;
             // 
             // botNine
             // 
             botNine.Font = new Font("Segoe UI", 15F);
             botNine.ForeColor = Color.Black;
-            botNine.Location = new Point(192, 100);
+            botNine.Location = new Point(136, 110);
             botNine.Name = "botNine";
             botNine.Size = new Size(56, 44);
             botNine.TabIndex = 14;
             botNine.Text = "9";
             botNine.UseVisualStyleBackColor = true;
+            botNine.MouseClick += botNine_MouseClick;
             // 
             // botEight
             // 
             botEight.Font = new Font("Segoe UI", 15F);
             botEight.ForeColor = Color.Black;
-            botEight.Location = new Point(130, 100);
+            botEight.Location = new Point(74, 110);
             botEight.Name = "botEight";
             botEight.Size = new Size(56, 44);
             botEight.TabIndex = 15;
             botEight.Text = "8";
             botEight.UseVisualStyleBackColor = true;
+            botEight.MouseClick += botEight_MouseClick;
             // 
             // botSeven
             // 
             botSeven.Font = new Font("Segoe UI", 15F);
             botSeven.ForeColor = Color.Black;
-            botSeven.Location = new Point(68, 100);
+            botSeven.Location = new Point(12, 110);
             botSeven.Name = "botSeven";
             botSeven.Size = new Size(56, 44);
             botSeven.TabIndex = 16;
             botSeven.Text = "7";
             botSeven.UseVisualStyleBackColor = true;
+            botSeven.MouseClick += botSeven_MouseClick;
             // 
             // botZero
             // 
             botZero.Font = new Font("Segoe UI", 15F);
             botZero.ForeColor = Color.Black;
-            botZero.Location = new Point(130, 250);
+            botZero.Location = new Point(74, 260);
             botZero.Name = "botZero";
             botZero.Size = new Size(56, 44);
             botZero.TabIndex = 17;
@@ -238,19 +256,70 @@
             // 
             button16.Font = new Font("Segoe UI", 15F);
             button16.ForeColor = Color.Black;
-            button16.Location = new Point(68, 250);
+            button16.Location = new Point(256, 111);
             button16.Name = "button16";
-            button16.Size = new Size(56, 44);
+            button16.Size = new Size(52, 44);
             button16.TabIndex = 18;
             button16.Text = "C";
             button16.UseVisualStyleBackColor = true;
             button16.MouseClick += button16_MouseClick;
             // 
+            // open
+            // 
+            open.Font = new Font("Segoe UI", 15F);
+            open.ForeColor = Color.Black;
+            open.Location = new Point(136, 260);
+            open.Name = "open";
+            open.Size = new Size(56, 44);
+            open.TabIndex = 19;
+            open.Text = "(";
+            open.UseVisualStyleBackColor = true;
+            open.MouseClick += open_MouseClick;
+            // 
+            // close
+            // 
+            close.Font = new Font("Segoe UI", 15F);
+            close.ForeColor = Color.Black;
+            close.Location = new Point(196, 260);
+            close.Name = "close";
+            close.Size = new Size(52, 44);
+            close.TabIndex = 20;
+            close.Text = ")";
+            close.UseVisualStyleBackColor = true;
+            close.MouseClick += close_MouseClick;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(198, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(52, 44);
+            button1.TabIndex = 21;
+            button1.Text = "←";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseClick += button1_MouseClick;
+            // 
+            // decButton
+            // 
+            decButton.Font = new Font("Segoe UI", 15F);
+            decButton.ForeColor = Color.Black;
+            decButton.Location = new Point(12, 260);
+            decButton.Name = "decButton";
+            decButton.Size = new Size(56, 44);
+            decButton.TabIndex = 22;
+            decButton.Text = ".";
+            decButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(395, 316);
+            ClientSize = new Size(326, 316);
+            Controls.Add(decButton);
+            Controls.Add(button1);
+            Controls.Add(close);
+            Controls.Add(open);
             Controls.Add(button16);
             Controls.Add(botZero);
             Controls.Add(botSeven);
@@ -294,5 +363,9 @@
         private Button botSeven;
         private Button botZero;
         private Button button16;
+        private Button open;
+        private Button close;
+        private Button button1;
+        private Button decButton;
     }
 }
--- Practice/Form1.cs
+++ Practice/Form1.cs
@@ -1,7 +1,11 @@
+using System.Data;
+
 namespace Practice
 {
     public partial class Form1 : Form
     {
+        private string _currentInput = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -9,17 +13,130 @@ namespace Practice
 
         private void botZero_MouseClick(object sender, MouseEventArgs e)
         {
-
+            UpdateInput("0");
         }
 
-        private void button5_MouseClick(object sender, MouseEventArgs e)
+        private void botOne_MouseClick(object sender, MouseEventArgs e)
         {
-            lblTest.Text = Convert.ToString(Convert.ToInt32(txtTest.Text));
+            UpdateInput("1");
+        }
+
+        private void botTwo_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("2");
+        }
+
+        private void botThree_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("3");
+        }
+
+        private void botFour_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("4");
+        }
+
+        private void botFive_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("5");
+        }
+
+        private void botSix_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("6");
+        }
+
+        private void botSeven_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("7");
+        }
+
+        private void botEight_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("8");
+        }
+
+        private void botNine_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("9");
+        }
+
+        private void add_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("+");
+        }
+
+        private void minus_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("-");
+        }
+
+        private void multiply_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("*");
+        }
+
+        private void divide_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("/");
+        }
+
+        private void open_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput("(");
+        }
+
+        private void close_MouseClick(object sender, MouseEventArgs e)
+        {
+            UpdateInput(")");
         }
 
         private void button16_MouseClick(object sender, MouseEventArgs e)
         {
+            txtTest.Text = string.Empty;
+            _currentInput = "";
+        }
 
+        private void button5_MouseClick(object sender, MouseEventArgs e)
+        {
+            CalculateResult();
+        }
+
+        private void UpdateInput(string value)
+        {
+            _currentInput += value;
+            txtTest.Text += value;
+        }
+
+        private void CalculateResult()
+        {
+            try
+            {
+                var result = EvaluateExpression(_currentInput);
+                lblTest.Text = result.ToString();
+                _currentInput = ""; // Clear the current input after calculation
+                txtTest.Clear(); // Clear the TextBox for new input
+            }
+            catch (Exception)
+            {
+                lblTest.Text = "Error"; // Handle any errors during evaluation
+            }
+        }
+
+        private double EvaluateExpression(string expression)
+        {
+            var dataTable = new DataTable();
+            var result = dataTable.Compute(expression, "");
+            return Convert.ToDouble(result);
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtTest.Text.Length > 0)
+            {
+                txtTest.Text = txtTest.Text.Remove(txtTest.Text.Length - 1, 1);
+                _currentInput = _currentInput.Remove(_currentInput.Length - 1, 1);
+            }
         }
     }
 }
